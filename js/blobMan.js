7dc6f666fbb95cf2a5cf6098984beefc887dccff
Johan <JohanSundin@bitbucket.org>
2014-12-15 21:21:31 +0100

    draw canvas

--- js/blobMan.js
+++ js/blobMan.js
@@ -1,16 +1,16 @@
 //blobMan.js
 function redrawScrean(maps, odata, blobCanvas){
-    console.log(odata);
     var baseImgData = odata[0];
 
 	result_canvas.width = baseImgData.width;
     result_canvas.height = baseImgData.height;
     var result_ctx = result_canvas.getContext("2d");
+    var canvas2 = document.createElement('CANVAS');//createcanvas(id);
+    // document.body.appendChild(canvas2);
+    canvas2.width =  baseImgData.width;
+    canvas2.height =  baseImgData.height;
+    var ctx2 = canvas2.getContext('2d');
   
-   
-    // var imageDatar = result_ctx.getImageData(0, 0, baseImgData.width, baseImgData.height);
-    result_ctx.globalAlpha = 0.5;
-
     var imageDatar = result_ctx.createImageData(baseImgData.width, baseImgData.height);
 
     // for (var yi = 0; yi < maps.length; yi++){
@@ -29,23 +29,16 @@ function redrawScrean(maps, odata, blobCanvas){
                     imageDatar.data[dptr + 2] = 0;//odata[maps[yi][1]].data[dptr + 2]  ;//>> 4;
                     imageDatar.data[dptr + 3] = 200;
                 }
-                // else{
-                //     imageDatar.data[dptr + 3] = 128;    
-                // }
             }            
         }
     }
-// result_ctx.globalAlpha = 1;
- // result_ctx.putImageData(baseImgData, 0, 0);
-    result_ctx.putImageData(imageDatar, 0, 0);
-    var canvas2 = document.createElement('CANVAS');//createcanvas(id);
-    document.body.appendChild(canvas2);
-    
-    canvas2.width =  baseImgData.width;
-    canvas2.height =  baseImgData.height;
-    var ctx2 = canvas2.getContext('2d');
-    ctx2.putImageData(baseImgData, 0, 0);
-    ctx2.drawImage(result_canvas,0,0);
+    // result_ctx.putImageData(imageDatar, 0, 0);
+    // ctx2.putImageData(baseImgData, 0, 0);
+    // ctx2.drawImage(result_canvas,0,0);
+
+    ctx2.putImageData(imageDatar, 0, 0);
+    result_ctx.putImageData(baseImgData, 0, 0);
+    result_ctx.drawImage(canvas2,0,0);
 
     //print(blobImage.blobs.data, blobImage.height);
 }
@@ -57,37 +50,38 @@ function zeros(size) {
     }
     return array;
 };
+
 ///////////////////////////////////////////////////////
-function blobMan(blobImage, baseImage, blobCanvas){
-	blobCanvas.width = blobImage.width;
-	blobCanvas.height = blobImage.height;
-	var blobCtx = blobCanvas.getContext("2d");
-	blobCtx.putImageData(baseImage, 0, 0);
+// function blobMan(blobImage, baseImage, blobCanvas){
+// 	blobCanvas.width = blobImage.width;
+// 	blobCanvas.height = blobImage.height;
+// 	var blobCtx = blobCanvas.getContext("2d");
+// 	blobCtx.putImageData(baseImage, 0, 0);
 
-    var canvas2 = document.createElement("canvas");
-    canvas2.width = blobImage.width;
-    canvas2.height = blobImage.height;
-    var ctx2 = canvas2.getContext("2d");
-	var imageDatar = ctx2.getImageData(0, 0, blobImage.width, blobImage.height);//blobCtx.getImageData(0, 0, blobImage.width, blobImage.height);
+//     var canvas2 = document.createElement("canvas");
+//     canvas2.width = blobImage.width;
+//     canvas2.height = blobImage.height;
+//     var ctx2 = canvas2.getContext("2d");
+// 	var imageDatar = ctx2.getImageData(0, 0, blobImage.width, blobImage.height);//blobCtx.getImageData(0, 0, blobImage.width, blobImage.height);
 	
-	var dptr = 0, dptr_s = 0;
-	for (var y = 0; y < blobImage.height; y++) {
-		for (var x = 0; x < blobImage.width; x++, dptr+=4, dptr_s+=1) {
-        	if(blobImage.blobs.data[dptr_s] !== 0){
-	        	imageDatar.data[dptr] = blobImage.data[dptr];
-				imageDatar.data[dptr + 1] = 0;//blobImage.data[dptr + 1];
-				imageDatar.data[dptr + 2] = 0;//blobImage.data[dptr + 2];
-				imageDatar.data[dptr + 3] = 200;
-        	}
-        	else{
-        		imageDatar.data[dptr + 3] = 0;	
-        	}			
-		}
-	}
+// 	var dptr = 0, dptr_s = 0;
+// 	for (var y = 0; y < blobImage.height; y++) {
+// 		for (var x = 0; x < blobImage.width; x++, dptr+=4, dptr_s+=1) {
+//         	if(blobImage.blobs.data[dptr_s] !== 0){
+// 	        	imageDatar.data[dptr] = blobImage.data[dptr];
+// 				imageDatar.data[dptr + 1] = 0;//blobImage.data[dptr + 1];
+// 				imageDatar.data[dptr + 2] = 0;//blobImage.data[dptr + 2];
+// 				imageDatar.data[dptr + 3] = 200;
+//         	}
+//         	else{
+//         		imageDatar.data[dptr + 3] = 0;	
+//         	}			
+// 		}
+// 	}
 
-    // put the modified pixels on the temporary canvas
-    ctx2.putImageData(imageDatar, 0, 0);
+//     // put the modified pixels on the temporary canvas
+//     ctx2.putImageData(imageDatar, 0, 0);
 
-    blobCtx.drawImage(canvas2, 0, 0);
-    // blobCtx.putImageData(imageDatar, 0, 0);
-}
+//     blobCtx.drawImage(canvas2, 0, 0);
+//     // blobCtx.putImageData(imageDatar, 0, 0);
+// }
